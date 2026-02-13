c078c74ec24253b65074cd1265b386f27596b7be
Valentin Nuñez <127317122+valentin21103@users.noreply.github.com>
2026-02-13 13:24:00 -0300

    update 1.0.2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,12 @@ import { HeaderComponent } from "./layout/header/header.component";
 import { FooterComponent } from "./layout/footer/footer.component";
 import { HomeComponent } from "./Pages/home/home.component";
 import { BlueSectionComponent } from "./Sections/blue-section/blue-section.component";
+import { ProyectosComponent } from "./Sections/proyectos/proyectos.component";
+import { ServiciosComponent } from "./Sections/servicios/servicios.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, HomeComponent, BlueSectionComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, HomeComponent, BlueSectionComponent, ProyectosComponent, ServiciosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
