5f94c531ea932f758d87fd1cbae64a6d24e4a9fe
Valentin Nuñez <127317122+valentin21103@users.noreply.github.com>
2026-02-20 13:32:46 -0300

    update nav-responsive

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,19 +1,30 @@
 import { Component, HostListener } from '@angular/core';
-import { FooterComponent } from "../footer/footer.component";
-import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
   isScrolled = false;
+  menuAbierto = false; // <-- Controla si el menú móvil está abierto o cerrado
 
   @HostListener('window:scroll', [])
   onWindowScroll() {
     this.isScrolled = window.scrollY > 50;
+  }
+
+  // Abre o cierra el menú al tocar las 3 rayitas
+  toggleMenu() {
+    this.menuAbierto = !this.menuAbierto;
+  }
+
+  // Cierra el menú automáticamente cuando el usuario toca un link
+  cerrarMenu() {
+    this.menuAbierto = false;
   }
 }
