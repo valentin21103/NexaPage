5cef6b91774abea06ec26e2d25789c1d5a3d527a
Valentin Nuñez <127317122+valentin21103@users.noreply.github.com>
2026-02-21 03:44:33 -0300

    Update 1.0.6

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
-import { BlueSectionComponent } from "../../Sections/blue-section/blue-section.component";
-import { ProyectosComponent } from "../../Sections/proyectos/proyectos.component";
-import { ServiciosComponent } from "../../Sections/servicios/servicios.component";
-import { RouterLink } from "@angular/router";
+import { CommonModule } from '@angular/common';
+import { BlueSectionComponent } from '../../Sections/blue-section/blue-section.component';
+import { ProyectosComponent } from '../../Sections/proyectos/proyectos.component';
+import { ServiciosComponent } from '../../Sections/servicios/servicios.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [BlueSectionComponent, ProyectosComponent, ServiciosComponent, RouterLink],
+  standalone: true,
+  imports: [CommonModule,BlueSectionComponent, ProyectosComponent, ServiciosComponent, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  // Se eliminó la lógica de videoIniciado ya que el iframe es directo
 }
