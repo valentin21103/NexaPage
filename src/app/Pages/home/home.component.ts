75481b7d0df864b0c87c8c18678acf415d0bce66
Valentin Nuñez <127317122+valentin21103@users.noreply.github.com>
2026-02-21 02:04:41 -0300

    fix-home-responsive

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { BlueSectionComponent } from "../../Sections/blue-section/blue-section.component";
 import { ProyectosComponent } from "../../Sections/proyectos/proyectos.component";
 import { ServiciosComponent } from "../../Sections/servicios/servicios.component";
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-home',
-  imports: [BlueSectionComponent, ProyectosComponent, ServiciosComponent],
+  imports: [BlueSectionComponent, ProyectosComponent, ServiciosComponent, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
