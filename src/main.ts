82daf5d325ab2a908aecc07484f50df6dc60d050
Valentin Nuñez <127317122+valentin21103@users.noreply.github.com>
2026-02-23 11:46:03 -0300

    Verce-NPM

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { injectSpeedInsights } from '@vercel/speed-insights';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent)
+  .then(() => {
+    injectSpeedInsights();
+  })
+  .catch(err => console.error(err));
