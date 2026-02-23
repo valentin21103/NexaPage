563051aa2cd9fadb8d58cd1a8bd8ccb4e9da9f09
Valentin Nuñez <127317122+valentin21103@users.noreply.github.com>
2026-02-23 11:39:31 -0300

    Favicon-icon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./layout/header/header.component";
 import { FooterComponent } from "./layout/footer/footer.component";
-import { HomeComponent } from "./Pages/home/home.component";
-import { BlueSectionComponent } from "./Sections/blue-section/blue-section.component";
-import { ProyectosComponent } from "./Sections/proyectos/proyectos.component";
-import { ServiciosComponent } from "./Sections/servicios/servicios.component";
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, HomeComponent, BlueSectionComponent, ProyectosComponent, ServiciosComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
